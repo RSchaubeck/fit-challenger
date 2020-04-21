e0faaa962ff554c42045da84311d01867e56e5e6
Chanu Choi <cjchoi97@gmail.com>
2020-04-21 14:46:58 -0400

    auth and protected routes along with session actions

--- frontend/src/reducers/session_reducer.js
+++ frontend/src/reducers/session_reducer.js
@@ -1,4 +1,8 @@
-import { RECEIVE_USER_LOGOUT } from '../actions/session_actions';
+import { 
+  RECEIVE_USER_LOGOUT,
+  RECEIVE_CURRENT_USER,
+  RECEIVE_USER_SIGN_IN 
+} from '../actions/session_actions';
 
 const initialState = {
   isAuthenticated: false,
@@ -11,6 +15,17 @@ export default function(state = initialState, action) {
       return {
         isAuthenticated: false,
         user: undefined
+      };
+    case RECEIVE_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: !!action.currentUser,
+        user: action.currentUser
+      };
+    case RECEIVE_USER_SIGN_IN:
+      return {
+        ...state,
+        isSignedIn: true
       };
     default:
       return state;
