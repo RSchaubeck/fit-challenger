7d42ffb2b1fb05d2303f967b78977a5dae93bc16
App Academy Student <email@email.com>
2020-04-22 15:18:15 -0400

    adding group challenges

--- models/Challenge.js
+++ models/Challenge.js
@@ -14,10 +14,10 @@ const ChallengeSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: 'users'
   },
-  challengee_id : {
+  challengee_id : [{
     type: Schema.Types.ObjectId,
     ref: 'users'
-  },
+  }],
   author_start_cals: {
     type: Number,
     ref: 'users',
--- routes/api/challenges.js
+++ routes/api/challenges.js
@@ -18,27 +18,11 @@ router.post("/",
     author_id: req.user.id,
     category: req.body.category,
     goal: req.body.goal,
-    //im not sure if we need to do searching for the person by their username here or in front end calcs
     challengee_id: req.body.challengee_id,
-    author_start_cals: req.user.calories_count,
+    author_start_cals: req.user.calories_count
   });
   newChallenge.save().then(challenge => res.json(challenge));
 });
-
-// router.get('/user/author/:author_id', (req, res) => {
-//   Challenge.find({author_id: req.params.author_id})
-//   .then(challenges => {
-//     if (challenges.length === 0){
-//       return res.json("You haven't Challenged anyone yet");
-//     }
-//     return res.json(challenges);
-//   })
-    
-    
-//   .catch(err => res.status(404).json(err));
-  
-// });
-
 
 router.get('/user/author', passport.authenticate("jwt", {session:false}), (req, res) => {
   Challenge.find({author_id: req.user.id})
@@ -78,7 +62,13 @@ router.patch('/user/accept/:chall_id', passport.authenticate("jwt", {session:fal
     .catch(err => res.status(404).json(err));
   });
 
+  router.get('/all_challenges', passport.authenticate("jwt", { session: false }), (req, res) =>{
+    Challenge.find()
+      .sort({ creation: -1})
+      .then(challenges => res.json(challenges))
+      .catch(err => res.status(404).json({err}));
 
+  });
 
 
 module.exports = router;
