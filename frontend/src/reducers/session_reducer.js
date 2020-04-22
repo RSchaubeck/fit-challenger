7aa6954861447e8c87d2bcc43f5229cd966be57c
Christina <huleechristina@gmail.com>
2020-04-22 14:04:44 -0400

    css style

---
+++ frontend/src/reducers/session_reducer.js
@@ -0,0 +1,34 @@
+import { 
+  RECEIVE_USER_LOGOUT,
+  RECEIVE_CURRENT_USER,
+  RECEIVE_USER_SIGN_IN 
+} from '../actions/session_actions';
+
+const initialState = {
+  isAuthenticated: false,
+  user: {}
+}
+
+export default function(state = initialState, action) {
+  switch(action.type) {
+    case RECEIVE_USER_LOGOUT:
+      return {
+        isAuthenticated: false,
+        user: undefined
+      };
+    case RECEIVE_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: !!action.currentUser,
+        user: action.currentUser
+      };
+    case RECEIVE_USER_SIGN_IN:
+      debugger
+      return {
+        ...state,
+        isSignedIn: true
+      };
+    default:
+      return state;
+  }
+}
