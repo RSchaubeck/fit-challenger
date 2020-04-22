b3288b7e16ab3814d108d9e7916a4ce15a6fd98e
Christina <huleechristina@gmail.com>
2020-04-22 15:01:54 -0400

    set up splash page

--- frontend/src/components/session/signup_form.jsx
+++ frontend/src/components/session/signup_form.jsx
@@ -43,13 +43,11 @@ class SignupForm extends React.Component {
       height: this.state.height,
       weight: this.state.weight
     };
-    debugger
 
     this.props.signup(user, this.props.history);
   }
 
   renderErrors() {
-    // debugger
     return (
       <ul className="errors">
         {Object.keys(this.state.errors).map((error, i) => (
