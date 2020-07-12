60d192100876c7b11451ba5008d2640545c0ff8e
Chanu Choi <cjchoi97@gmail.com>
2020-07-12 16:12:40 -0400

    unkown

--- frontend/src/components/session/login_form.jsx
+++ frontend/src/components/session/login_form.jsx
@@ -19,7 +19,7 @@ class LoginForm extends React.Component {
     this.renderErrors = this.renderErrors.bind(this);
   }
 
-  // Once the user has been authenticated, redirect to the Tweets page
+  // Once the user has been authenticated, redirect to the Challenges page
   componentWillReceiveProps(nextProps) {
     if (nextProps.currentUser === true) {
       this.props.history.push("/challenges");
